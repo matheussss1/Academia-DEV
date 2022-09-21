dcf9b181471ba72f27a94091ef139f22f821c12c
Stags <matheusfelipe_contato@hotmail.com>
2022-09-21 14:18:31 -0300

    feat: exercicios dia 04

---
+++ Javascript/04/1_0_exercicios.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * 1 - Dado uma string com uma frase informada pelo usuário
+ *     (incluindo espaços em branco) conte:
+ * 
+ *       a) quantos espaços em branco existem na frase.
+ *       b) quantas vezes aparecem as vogais a, e, i, o, u.
+ * 
+ * 2 - Escreva um programa que pergunte um número n para o usuário e 
+ *     exiba todos os seus divisores.
+ * 
+ * 3 - Escreva um programa que recebe uma frase do usuário e exibe
+ *    a mesma frase sem nenhuma vogal
+ * 
+ * 4 - Utilizando a estrutura de repetição for, faça um programa
+ *   que receba 10 números e conte quantos deles estão entre o intervalo 10 e 20
+ *   e quantos deles estão fora do intervalo, escrevendo estas informações.
+ * 
+ * 
+ * 5 - Escrever um algoritmo que leia uma quantidade desconhecida de números e conte quantos deles estão nos seguintes intervalos:
+ *     [0-25], [26-50], [51-75] e [76-100].
+ *     A entrada de dados deve terminar quando for lido um número negativo.
+ * 
+ * 7 - Faça um programa que funcione como uma calculadora.
+ * O Programa deve apresentar um menu ao usuário da seguinte forma:
+ * 
+ *  1 - Somar
+ *  2 - Subtrair
+ *  3 - Multiplicar
+ *  4 - Dividir
+ *  0 - Sair
+ * 
+ * Ao escolher uma opção peça dois numeros e realize a operação
+ * 
+ * 6 - Faça um jogo da velha em javascript no terminal
+ * 
+ */
